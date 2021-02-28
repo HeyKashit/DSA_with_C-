b53c55e9a9963b76640af78d30866c05489477b9
Kashit Duhan <75305061+HeyKashit@users.noreply.github.com>
2021-02-28 11:19:29 +0530

    Update tree2.cpp

--- tree2.cpp
+++ tree2.cpp
@@ -43,3 +43,57 @@ int main()
     
     return 0;
 }
+
+
+
+
+
+// big tree.
+
+#include<iostream>
+#include<vector> 
+using namespace std;
+class Treenode{
+    public:
+    int data;
+    vector<Treenode*> children;
+    // making constructor for the data entery;
+    Treenode(int d)
+    {
+        this->data=d;
+    }
+};
+
+void print_tree_node(Treenode*root)
+{
+    cout<<root->data<<" : ";
+    for(int i=0;i<root->children.size();i++)
+    {
+        cout<<root->children[i]->data<<" , ";
+    }
+    cout<<endl;
+    for(int i=0;i<root->children.size();i++)
+    {
+        print_tree_node(root->children[i]);
+    }
+}
+
+int main()
+{
+    Treenode * root  = new Treenode(10);
+    Treenode * node1 = new Treenode(20);
+    Treenode * node2 = new Treenode(30);
+    Treenode * node3 = new Treenode(40);
+    Treenode * node4 = new Treenode(50);
+    Treenode * node5 = new Treenode(60);
+    
+    root->children.push_back(node1);
+    root->children.push_back(node2);
+    root->children.push_back(node3);
+    
+    node1->children.push_back(node4);
+    node1->children.push_back(node5);
+    
+    print_tree_node(root);
+    
+}
